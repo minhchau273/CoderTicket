1a6265b547b1b7b5688943d3960b3fe3b489a3c2
Chau Vo <minhchau273@gmail.com>
2016-07-26 09:30:19 +0700

    [r] Use strong params in session_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
+    @user = User.find_by_email(session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to '/'
     else
@@ -21,5 +21,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
