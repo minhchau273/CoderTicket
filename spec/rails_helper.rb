a5e17a2b7b4a487cc989325ede588b81e530547b
Chau Vo <minhchau273@gmail.com>
2016-07-26 10:05:02 +0700

    [r] Use double quote instead of single quote

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'factory_girl_rails'
+require "spec_helper"
+require "rspec/rails"
+require "factory_girl_rails"
 
 ActiveRecord::Migration.maintain_test_schema!
 
