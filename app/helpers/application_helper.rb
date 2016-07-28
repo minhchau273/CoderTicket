98236d58c8a1c22dec3c9f91d074c0e7af506c3f
Chau Vo <minhchau273@gmail.com>
2016-07-28 11:22:28 +0700

    [r] Fix typo “formated_price”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def formated_price(price)
+  def formatted_price(price)
     "#{number_with_precision(price, precision: 0, delimiter: ',')} VND"
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "formated_price" do
+  describe "formatted_price" do
     let(:price) { 1000000 }
     let(:expected_price) { "1,000,000 VND" }
     it "returns this price with vietnamese format" do
-      expect(formated_price(price)).to eq expected_price
+      expect(formatted_price(price)).to eq expected_price
     end
   end
 end
