c23d697252de6b1355c385dc03a12d317ea31dc5
Chau Vo <minhchau273@gmail.com>
2016-07-28 15:19:23 +0700

    [t] Test Show action in events_controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe EventsController, type: :controller do
       expect(assigns(:events)).to eq [event_3, event_1]
     end
   end
+
+  describe "GET #show" do
+    let!(:event) { create(:event) }
+
+    before do
+      get :show, id: event.id
+    end
+
+    it "is success" do
+      expect(response).to be_success
+      expect(assigns(:event)).to eq event
+    end
+  end
 end
