c66c4c3e85da84f1cb757a330c9e5fe6993a140c
Chau Vo <minhchau273@gmail.com>
2016-07-29 16:36:04 +0700

    [f] Search upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
   def index
     @events = Event.upcoming
+    if (@keyword = params["search"]).present?
+      keywords = @keyword.downcase.split.join("|")
+      where_clause = "lower(name) similar to '%(#{keywords})%'"
+      @events = @events.where(where_clause)
+    end
   end
 
   def show
