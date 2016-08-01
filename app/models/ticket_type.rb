88c00a01ed12d19aef7ed2a56f865e3208fd779d
Chau Vo <minhchau273@gmail.com>
2016-08-01 14:28:21 +0700

    [f] Set default value for remain in ticket_type model

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -3,4 +3,16 @@ class TicketType < ActiveRecord::Base
 
   validates :name, :price, :max_quantity, presence: true
   validates :name, uniqueness: true
+
+  before_create :default_remain
+
+  def actual_max_quantity
+    remain < MAX_QUANTITY ? remain : MAX_QUANTITY
+  end
+
+  private
+
+  def default_remain
+    self.remain ||= max_quantity
+  end
 end
--- spec/models/ticket_type_spec.rb
+++ spec/models/ticket_type_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe TicketType, type: :model do
     it { is_expected.to validate_presence_of :max_quantity }
     it { is_expected.to validate_uniqueness_of :name }
   end
+
+  describe "before create #set_remain" do
+    let!(:type) { create(:ticket_type) }
+
+    it "sets max quantity value for remain" do
+      expect(type.remain).to eq type.max_quantity
+    end
+  end
+
+  describe "#actual_max_quantity" do
+    subject { type.actual_max_quantity }
+
+    let(:type) { create(:ticket_type, max_quantity: remain) }
+
+    context "the remain is less than the max quantity" do
+      let(:remain) { 5 }
+
+      it { is_expected.to eq remain }
+    end
+
+    context "the remain is not less than the max quantity" do
+      let(:remain) { 10 }
+
+      it { is_expected.to eq remain }
+    end
+  end
 end
